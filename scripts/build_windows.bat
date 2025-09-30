9768777791b6575c3e7b1a5bb8ec46869ba23d9d
Robin Naumann <constorux@gmail.com>
2025-09-30 10:04:32 +0200

    feat: beta windows release version

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -2,8 +2,15 @@
 
 
 ::delete dist folder
-rmdir /s /q dist
+rmdir /s /q dist/windows
 
 echo "Windows: 1/3: apply plugins..."
+call flutter pub get
+call dart run flutter_launcher_icons
+call dart run change_app_package_name:main $package
+ 
+echo "Windows: 2/3: building application..."
+call flutter build windows --release
 
+echo "Windows: 3/3: building installer..."
 call "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" ./scripts/windows_installer.iss
